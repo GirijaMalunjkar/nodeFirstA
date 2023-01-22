9a23a98167cc9b4f454155318e5c1e27c5f2e2c6
Girija Malunjkar <girijamalunjkar666@gmail.com>
2023-01-22 15:23:20 +0530

    update command line

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 var Express = required'express';
 var app = express();
 var port = process.env.PORT;
+    app.listen(port,function(err){
+        console.log('The server is running on port : ' =port);
+    })
